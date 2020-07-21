095f0fbe5da659251a8dea8c8b81b9fe654eded2
EldenSokolaj <56181940+EldenSokolaj@users.noreply.github.com>
2020-07-20 21:30:46 -0500

    Create about.php

---
+++ options/about.php
@@ -0,0 +1,16 @@
+<html>
+  <style>
+ body { background-image: url("../background.jpg"); }
+  </style>
+<head>
+    <link rel="stylesheet" href="../alert.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Troop 90 | Geocach | Blank</title>
+</head>
+<body text="white">
+<h4>
+Yesterday night at our troop meeting our plc introduced a geocache to our scouts. A geocache is like a scavenger hunt where one clue leads to another clue. This geocache will take place at the brookfield circle starting today thursday july 16 at 12 pm, and ending sunday night. The geocache will take approximately 30 minutes and is located entirely within 8 corners. The clues will be in tiny tupperware containers. Scouts will need a phone or a notebook in order to record the passwords found at each clue.  Once your scout finds a clue they should write down the code or submit it to the website provided down below. The website is used to track your scouts progress and enter them into a raffle for a 5 dollar gift card! Additionally a pizza party will be given to the patrol who gets the highest participation in the geocache. If you have any questions or concerns please don't hesitate to ask in the #get-help chat in slack.
+Your First Clue is  “Don’t Forgot To Lock Your Bike”
+</h4>
+</body>
+</html>
